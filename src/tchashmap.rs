333a037f38172bab0fcc53d829fca143e805a720
acrimon <joel.wejdenstal@gmail.com>
2019-03-25 21:37:23 +0100

    Continued work on TCHashMap. Running into some obscure bugs.

--- src/tchashmap.rs
+++ src/tchashmap.rs
@@ -3,14 +3,13 @@
 use swym::thread_key;
 use swym::tcell::{TCell, Ref};
 use std::hash::Hash;
-use std::marker::PhantomData;
 // use swym::{ReadTx, RWTx};
 use swym::tx::{Borrow, Ordering};
 use std::sync::atomic;
 use parking_lot::RwLock;
 use std::mem;
 
-pub const HASHMAP_INIT_CAPACITY_LOOKUP_BITS: usize = 4;
+pub const HASHMAP_INIT_LOOKUP_BITS: usize = 4;
 pub const HASHMAP_LOAD_THRESHOLD: f32 = 0.5;
 
 #[inline]
@@ -32,95 +31,95 @@ pub enum Entry<K: Send + Sized + Clone + Hash, V: Send + Sized + Clone> {
 
 unsafe impl<K: Send + Sized + Clone + Hash, V: Send + Sized + Clone> Borrow for Entry<K, V> {}
 
-pub struct TCHashMap<K, V>
+struct TCHashMapRaw<K, V>
 where
-    K: Send + Clone + Hash + ?Sized,
+    K: Send + Clone + Hash + Sized,
     V: Send + Clone,
 {
-    global_lock: atomic::AtomicBool,
-    storage: RwLock<Vec<TCell<Entry<K, V>>>>,
-    len: atomic::AtomicUsize,
-    lookup_bits_count: atomic::AtomicUsize,
-    capacity: atomic::AtomicUsize,
-    phantom: PhantomData<K>,
+    pub storage: Vec<TCell<Entry<K, V>>>,
+    pub len: atomic::AtomicUsize,
+    pub lbc: usize,
+    pub capacity: usize,
+}
+
+pub struct TCHashMap<K, V>
+where
+    K: Send + Clone + Hash + Sized,
+    V: Send + Clone,
+{
+    inner: RwLock<TCHashMapRaw<K, V>>,
 }
 
 impl<K: 'static, V> TCHashMap<K, V>
 where
-    K: Send+ Clone + Hash + ?Sized,
+    K: Send+ Clone + Hash + Sized,
     V: Send + 'static + Clone,
 {
-    #[inline]
     pub fn new() -> Self {
-        let capacity = 2_usize.pow(HASHMAP_INIT_CAPACITY_LOOKUP_BITS as u32);
+        let capacity = 2_usize.pow(HASHMAP_INIT_LOOKUP_BITS as u32);
 
         Self {
-            global_lock: atomic::AtomicBool::new(false),
-            storage: RwLock::new(init_storage::<K, V>(capacity)),
-            len: atomic::AtomicUsize::new(0),
-            lookup_bits_count: atomic::AtomicUsize::new(HASHMAP_INIT_CAPACITY_LOOKUP_BITS),
-            capacity: atomic::AtomicUsize::new(capacity),
-            phantom: PhantomData,
+            inner: RwLock::new(TCHashMapRaw {
+                storage: init_storage::<K, V>(capacity),
+                len: atomic::AtomicUsize::new(0),
+                lbc: HASHMAP_INIT_LOOKUP_BITS,
+                capacity,
+            })
         }
     }
 
-    #[inline]
     pub fn resize(&self, lbc: usize) {
-        while self.global_lock.load(atomic::Ordering::Relaxed) {}
+        let mut inner = self.inner.write();
+        let new_capacity = 2_usize.pow(lbc as u32);
 
-        self.global_lock.store(true, atomic::Ordering::SeqCst);
-        let mut storage = self.storage.write();
-        self.capacity.fetch_update(|mut x| {
-            x *= 2;
-            Some(x)
-        }, atomic::Ordering::SeqCst, atomic::Ordering::SeqCst).unwrap();
-        self.lookup_bits_count.fetch_add(1, atomic::Ordering::SeqCst);
-        {
-            let old_storage = mem::replace(&mut *storage, init_storage(2_usize.pow(lbc as u32)));
-            old_storage.into_iter().for_each(|cell| {
-                if let Entry::Occupied(k, v) = cell.into_inner() {
-                    let hash = fxhash::hash32(&k);
-                    let index = compute_index(hash, lbc);
-                    storage[index] = TCell::new(Entry::Occupied(k, v));
-                }
-            });
-        }
-        self.global_lock.store(false, atomic::Ordering::SeqCst);
+        inner.lbc = lbc;
+        inner.capacity = new_capacity;
+
+        let new_buf = init_storage(new_capacity);
+        let old_buf = mem::replace(&mut inner.storage, new_buf);
+
+        old_buf.into_iter().for_each(|cell| {
+            if let Entry::Occupied(k, v) = cell.into_inner() {
+                let hash = fxhash::hash32(&k);
+                let index = compute_index(hash, lbc);
+                println!("index: {} hash: {}", index, hash);
+                inner.storage[index] = TCell::new(Entry::Occupied(k, v));
+            }
+        });
     }
 
-    #[inline]
     pub fn insert(&self, k: K, v: V) {
-        while self.global_lock.load(atomic::Ordering::Relaxed) {}
+        let inner = self.inner.read();
+        let len = inner.len.load(atomic::Ordering::Relaxed);
+        let lbc = inner.lbc;
+        let capacity = inner.capacity;
+        drop(inner);
 
-        let len = self.len.load(atomic::Ordering::Relaxed);
-        let capacity = self.len.load(atomic::Ordering::Relaxed);
-        let lbc = self.lookup_bits_count.load(atomic::Ordering::Relaxed);
+        println!("newlen: {}, thcap: {}", len + 1, (capacity as f32 * HASHMAP_LOAD_THRESHOLD) as usize);
         if len + 1 > (capacity as f32 * HASHMAP_LOAD_THRESHOLD) as usize {
             self.resize(lbc + 1);
         }
 
-        self.len.fetch_add(1, atomic::Ordering::Relaxed);
+        let inner = self.inner.read();
         let thread_key = thread_key::get();
         let hash = fxhash::hash32(&k);
-        let index = compute_index(hash, self.lookup_bits_count.load(atomic::Ordering::Relaxed));
-        let storage = self.storage.read();
-
+        let index = compute_index(hash, inner.lbc);
+        inner.len.fetch_add(1, atomic::Ordering::Relaxed);
         thread_key.rw(|tx| {
-            storage[index].set(tx, Entry::Occupied(k.clone(), v.clone()))?;
+            inner.storage[index].set(tx, Entry::Occupied(k.clone(), v.clone()))?;
             Ok(())
         });
     }
 
-    #[inline]
-    pub fn get_cloned(&self, k: &K) -> Entry<K, V> {
+    pub fn get_cloned(&self, k: &K) -> V {
+        let inner = self.inner.read();
         let thread_key = thread_key::get();
         let hash = fxhash::hash32(&k);
-        let index = compute_index(hash, self.lookup_bits_count.load(atomic::Ordering::Relaxed));
+        let index = compute_index(hash, inner.lbc);
         let mut v = None;
-        let storage = self.storage.read();
 
         thread_key.read(|tx| {
-            let inner: Result<Ref<Entry<K, V>>, ()> = Ok(storage[index].borrow(tx, Ordering::Read)?);
+            let inner: Result<Ref<Entry<K, V>>, ()> = Ok(inner.storage[index].borrow(tx, Ordering::Read)?);
             if let Ok(inner) = inner {
                 v = Some(inner.clone());
             }
@@ -128,16 +127,38 @@ where
             Ok(())
         });
 
-        v.expect("undefined error from swym")
+        if let Entry::Occupied(_, v) = v.expect("undefined error from swym") {
+            v
+        } else {
+            panic!()
+        }
     }
 }
 
 impl<K: 'static, V> Default for TCHashMap<K, V>
 where
-    K: Send + Clone + Hash + ?Sized,
+    K: Send + Clone + Hash + Sized,
     V: Send + 'static + Clone,
 {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_then_assert() {
+        let map = TCHashMap::new();
+        let n: u32 = 128;
+        (0..n).for_each(|i| {
+            map.insert(i, i + 9);
+        });
+
+        (0..n).for_each(|i| {
+            assert_eq!(i + 9, map.get_cloned(&i));
+        });
     }
 }
