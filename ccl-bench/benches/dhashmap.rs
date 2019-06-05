e1501552297cae5aa85d13e379086e2b8c20de52
Acrimon <joel.wejdenstal@gmail.com>
2019-06-06 00:21:55 +0200

    Rewrote crude benches.

--- ccl-bench/benches/dhashmap.rs
+++ ccl-bench/benches/dhashmap.rs
@@ -3,7 +3,6 @@ extern crate criterion;
 
 use rayon::prelude::*;
 use ccl::dhashmap::DHashMap;
-use ccl::experimental::crude::CrudeHashMap;
 use criterion::Criterion;
 use lazy_static::lazy_static;
 
@@ -16,23 +15,6 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 lazy_static! {
     static ref DATA1MAP: DHashMap<u64, [u128; 16]> = dhashmap_ccl_rayon_insert_only_100k_u64_u128x16();
     static ref DATA2MAP: DHashMap<u64, u64> = dhashmap_ccl_rayon_insert_only_100k_u64_u64();
-    static ref DATA3MAP: CrudeHashMap<u64, u64> = crudemap_ccl_rayon_insert_only_100k_u64_u64();
-}
-
-fn crudemap_ccl_rayon_insert_only_100k_u64_u64() -> CrudeHashMap<u64, u64> {
-    let map = CrudeHashMap::new(2048);
-
-    (0..100000_u64).into_par_iter().for_each(|i| {
-        map.insert(i, DATA2);
-    });
-
-    map
-}
-
-fn crudemap_ccl_rayon_read_only_100k_u64_u64(map: &CrudeHashMap<u64, u64>) {
-    (0..100000_u64).into_par_iter().for_each(|i| {
-        assert!(*map.get(&i).unwrap() != std::u64::MAX);
-    });
 }
 
 fn dhashmap_ccl_rayon_insert_only_100k_u64_u64() -> DHashMap<u64, u64> {
@@ -69,10 +51,8 @@ fn dhashmap_ccl_rayon_read_only_100k_u64_u128x16(map: &DHashMap<u64, [u128; 16]>
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("dhashmap_ccl_rayon_insert_only_100k_u64_u64", |b| b.iter(|| dhashmap_ccl_rayon_insert_only_100k_u64_u64()));
-    c.bench_function("crudemap_ccl_rayon_insert_only_100k_u64_u64", |b| b.iter(|| crudemap_ccl_rayon_insert_only_100k_u64_u64()));
     c.bench_function("dhashmap_ccl_rayon_insert_only_100k_u64_u128x16", |b| b.iter(|| dhashmap_ccl_rayon_insert_only_100k_u64_u128x16()));
     c.bench_function("dhashmap_ccl_rayon_read_only_100k_u64_u64", |b| b.iter(|| dhashmap_ccl_rayon_read_only_100k_u64_u64(&DATA2MAP)));
-    c.bench_function("crudemap_ccl_rayon_read_only_100k_u64_u64", |b| b.iter(|| crudemap_ccl_rayon_read_only_100k_u64_u64(&DATA3MAP)));
     c.bench_function("dhashmap_ccl_rayon_read_only_100k_u64_u128x16", |b| b.iter(|| dhashmap_ccl_rayon_read_only_100k_u64_u128x16(&DATA1MAP)));
 }
 
