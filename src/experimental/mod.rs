561c3db197c69d92b2bca59fb3de0ee5aba8d185
Acrimon <joel.wejdenstal@gmail.com>
2019-06-12 16:54:21 +0200

    Added experimental nested map.

--- src/experimental/mod.rs
+++ src/experimental/mod.rs
@@ -1,2 +1,1 @@
-pub mod crude;
 pub mod nestedmap;
--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,5 @@
 use std::hash::Hash;
 use std::hash::Hasher;
-use std::sync::atomic::{AtomicBool, Ordering};
-
-#[inline]
-pub fn hash<T: Hash>(v: &T) -> u64 {
-    let mut hash_state = fxhash::FxHasher64::default();
-    v.hash(&mut hash_state);
-    hash_state.finish()
-}
 
 #[inline]
 pub fn hash_with_nonce<T: Hash>(v: &T, nonce: u64) -> u64 {
@@ -15,13 +7,4 @@ pub fn hash_with_nonce<T: Hash>(v: &T, nonce: u64) -> u64 {
     hash_state.write_u64(nonce);
     v.hash(&mut hash_state);
     hash_state.finish()
-}
-
-#[inline]
-pub fn round_pow2(x: usize) -> usize {
-    let mut pow = 1;
-    while pow < x {
-        pow *= 2;
-    }
-    pow
 }
