e522f46104847b285ba3004d85b55aa4454f7ac8
Acrimon <joel.wejdenstal@gmail.com>
2019-06-12 18:33:11 +0200

    Fixed clippy warn

--- src/util.rs
+++ src/util.rs
@@ -11,7 +11,5 @@ pub fn hash_with_nonce<T: Hash>(v: &T, nonce: u64) -> u64 {
     let mut hash_state = fxhash::FxHasher64::default();
     hash_state.write_u64(result.wrapping_mul(nonce).wrapping_sub(44));
     hash_state.write_u64(nonce);
-    let result = hash_state.finish();
-
-    result
+    hash_state.finish()
 }
