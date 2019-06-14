83ee578fea2f89ec7361d3b371d7059100b78843
Acrimon <joel.wejdenstal@gmail.com>
2019-06-14 17:08:42 +0200

    Implemented a basic memory pool. Very slow at the moment.

--- src/uniform_allocator.rs
+++ src/uniform_allocator.rs
@@ -1,6 +1,7 @@
 use std::mem;
 use slab::Slab;
 use hashbrown::HashMap;
+use std::collections::VecDeque;
 
 #[derive(Hash, PartialEq, Eq)]
 struct ObjectKey(usize);
@@ -32,9 +33,49 @@ impl<T> SlabSegment<T> {
         ptr as *mut u8
     }
 
-    fn dealloc(&mut self, ptr: *mut u8) {
+    fn dealloc(&mut self, ptr: *mut u8) -> bool {
         let ptr = ptr as usize;
-        let key = self.mappings.remove(&Pointer(ptr)).unwrap();
-        self.objects.remove(key.0);
+        if let Some(key) = self.mappings.remove(&Pointer(ptr)) {
+            self.objects.remove(key.0);
+            true
+        } else {
+            false
+        }
+    }
+}
+
+struct MemoryPool<T> {
+    segments: VecDeque<SlabSegment<T>>,
+}
+
+impl<T> MemoryPool<T> {
+    fn new() -> Self {
+        Self {
+            segments: VecDeque::new(),
+        }
+    }
+
+    fn alloc(&mut self) -> *mut u8 {
+        let mut search_idx = 0;
+
+        loop {
+            if let Some(mut segment) = self.segments.get_mut(search_idx) {
+                if segment.has_space() {
+                    return segment.alloc();
+                } else {
+                    search_idx += 1;
+                }
+            } else {
+                self.segments.push_back(SlabSegment::new(64));
+            }
+        }
+    }
+
+    fn dealloc(&mut self, ptr: *mut u8) {
+        for segment in &mut self.segments {
+            if segment.dealloc(ptr) {
+                break;
+            }
+        }
     }
 }
