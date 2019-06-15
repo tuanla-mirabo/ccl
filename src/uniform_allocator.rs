7d6c76ef7afc9a4a5d6d60c21a774ee19a77775a
Acrimon <joel.wejdenstal@gmail.com>
2019-06-15 11:04:39 +0200

    Removed custom allocator for the moment and only kept the API. Going to rewrite the allocator later.

--- src/uniform_allocator.rs
+++ src/uniform_allocator.rs
@@ -1,10 +1,9 @@
-use hashbrown::HashMap;
-use parking_lot::Mutex;
-use slab::Slab;
-use std::collections::VecDeque;
-use std::mem;
+use std::alloc::{alloc, dealloc, Layout};
+use std::ptr;
+use std::marker::PhantomData;
+use std::sync::atomic::{Ordering, AtomicUsize};
 
-// TODO shrinking, optimization
+// TODO: write v3
 //
 //
 // v3 ideas:
@@ -18,138 +17,41 @@ use std::mem;
 //
 // Unrelated idea: cache parts in a threadlocal cache, this maybe be segments or whatever
 
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
-struct ObjectKey(usize);
-
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
-struct Pointer(usize);
-
-const SEGMENT_SIZE: usize = 128;
-const SEGMENT_MOVE_THRESHOLD: usize = SEGMENT_SIZE / 8;
-
-struct SlabSegment<T> {
-    objects: Slab<T>,
-    mappings: HashMap<Pointer, ObjectKey>,
-}
-
-impl<T> SlabSegment<T> {
-    #[inline]
-    fn new(capacity: usize) -> Self {
-        Self {
-            objects: Slab::with_capacity(capacity),
-            mappings: HashMap::with_capacity(capacity),
-        }
-    }
-
-    #[inline]
-    fn has_space(&self) -> usize {
-        self.objects.capacity() - self.objects.len()
-    }
-
-    #[inline]
-    fn alloc(&mut self) -> *mut u8 {
-        let key = self.objects.insert(unsafe { mem::uninitialized() });
-        let ptr = unsafe { self.objects.get_unchecked_mut(key) as *mut T as usize };
-        self.mappings.insert(Pointer(ptr), ObjectKey(key));
-        ptr as *mut u8
-    }
-
-    #[inline]
-    fn dealloc(&mut self, ptr: *mut u8) -> Option<T> {
-        let ptr = ptr as usize;
-        if let Some(key) = self.mappings.remove(&Pointer(ptr)) {
-            Some(self.objects.remove(key.0))
-        } else {
-            None
-        }
-    }
-}
-
-struct MemoryPool<T> {
-    segments: VecDeque<SlabSegment<T>>,
-}
-
-impl<T> MemoryPool<T> {
-    #[inline]
-    fn new() -> Self {
-        Self {
-            segments: VecDeque::new(),
-        }
-    }
-
-    #[inline]
-    fn alloc(&mut self) -> *mut u8 {
-        let mut search_idx = 0;
-
-        loop {
-            if let Some(segment) = self.segments.get_mut(search_idx) {
-                let space = segment.has_space();
-
-                if space != 0 {
-                    let alloc = segment.alloc();
-
-                    if search_idx > (self.segments.len() / 4) && space > SEGMENT_MOVE_THRESHOLD {
-                        let segment = self.segments.remove(search_idx).unwrap();
-                        self.segments.push_front(segment);
-                    }
-
-                    return alloc;
-                } else {
-                    search_idx += 1;
-                }
-            } else {
-                self.segments.push_front(SlabSegment::new(SEGMENT_SIZE));
-                search_idx = 0;
-            }
-        }
-    }
-
-    #[inline]
-    fn dealloc(&mut self, ptr: *mut u8) -> Option<T> {
-        for segment in &mut self.segments {
-            if let Some(v) = segment.dealloc(ptr) {
-                return Some(v);
-            }
-        }
-
-        panic!("invalid ptr on dealloc");
-    }
-}
-
 pub struct UniformAllocator<T> {
-    pool_count: usize,
-    pools: Box<[Mutex<MemoryPool<T>>]>,
+    ctr: Box<[AtomicUsize]>,
+    marker: PhantomData<T>,
 }
 
 impl<T> UniformAllocator<T> {
     #[inline]
-    pub fn new(pool_count: usize) -> Self {
+    pub fn new(count: usize) -> Self {
         Self {
-            pool_count,
-            pools: (0..pool_count)
-                .map(|_| Mutex::new(MemoryPool::new()))
+            ctr: (0..count)
+                .map(|_| AtomicUsize::new(0))
                 .collect::<Vec<_>>()
                 .into_boxed_slice(),
+            marker: PhantomData,
         }
     }
 
     #[inline]
     pub fn alloc(&self, tag: usize) -> *mut u8 {
-        let pool_idx = tag % self.pool_count;
-        let mut pool = self.pools[pool_idx].lock();
-        pool.alloc()
+        self.ctr[tag % self.ctr.len()].fetch_add(1, Ordering::Relaxed);
+        unsafe { alloc(Layout::new::<T>()) }
     }
 
     #[inline]
     pub fn dealloc(&self, tag: usize, ptr: *mut u8) -> Option<T> {
-        let pool_idx = tag % self.pool_count;
-        let mut pool = self.pools[pool_idx].lock();
-        pool.dealloc(ptr)
+        self.ctr[tag % self.ctr.len()].fetch_add(1, Ordering::Relaxed);
+        unsafe {
+            let data = ptr::read(ptr as *const _);
+            dealloc(ptr, Layout::new::<T>());
+            data
+        }
     }
 }
 
 impl<T> Default for UniformAllocator<T> {
-    #[inline]
     fn default() -> Self {
         Self::new(num_cpus::get() * 4)
     }
