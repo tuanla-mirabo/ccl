287232fb5cda1160bf38a75b78e4d9a7da9d642b
Acrimon <joel.wejdenstal@gmail.com>
2019-07-15 22:33:36 +0200

    Allocator fixes.

--- src/uniform_allocator.rs
+++ src/uniform_allocator.rs
@@ -1,48 +1,30 @@
+//! This is a thin allocator wrapper for use in some datastructures.
+//! This is here incase we want to implement a custom allocator at a later date.
+
 use std::alloc::{alloc, dealloc, Layout};
 use std::marker::PhantomData;
 use std::ptr;
-use std::sync::atomic::{AtomicUsize, Ordering};
 
-// TODO: write v3
-//
-//
-// v3 ideas:
-//
-// 1. linked list of segments, each containing a bitmap to keep track of whats allocated and an array of buckets, segments dont have to be a linked list
-// some kind of tree might work aswell
-//
-// 2. a stack containing pointers to free chunks within a segment
-// to allocate just pop the stack, to dealloc, push to the stack, have multiple segments
-//
-//
-// Unrelated idea: cache parts in a threadlocal cache, this maybe be segments or whatever
-
+/// Please see module level documentation.
 pub struct UniformAllocator<T> {
-    ctr: Box<[AtomicUsize]>,
     marker: PhantomData<T>,
 }
 
 impl<T> UniformAllocator<T> {
     #[inline]
-    pub fn new(count: usize) -> Self {
+    pub fn new() -> Self {
         Self {
-            ctr: (0..count)
-                .map(|_| AtomicUsize::new(0))
-                .collect::<Vec<_>>()
-                .into_boxed_slice(),
             marker: PhantomData,
         }
     }
 
     #[inline]
-    pub fn alloc(&self, tag: usize) -> *mut u8 {
-        self.ctr[tag % self.ctr.len()].fetch_add(1, Ordering::Relaxed);
+    pub fn alloc(&self, _tag: usize) -> *mut u8 {
         unsafe { alloc(Layout::new::<T>()) }
     }
 
     #[inline]
-    pub fn dealloc(&self, tag: usize, ptr: *mut u8) -> Option<T> {
-        self.ctr[tag % self.ctr.len()].fetch_add(1, Ordering::Relaxed);
+    pub fn dealloc(&self, _tag: usize, ptr: *mut u8) -> Option<T> {
         unsafe {
             let data = ptr::read(ptr as *const _);
             dealloc(ptr, Layout::new::<T>());
@@ -53,6 +35,6 @@ impl<T> UniformAllocator<T> {
 
 impl<T> Default for UniformAllocator<T> {
     fn default() -> Self {
-        Self::new(num_cpus::get() * 4)
+        Self::new()
     }
 }
